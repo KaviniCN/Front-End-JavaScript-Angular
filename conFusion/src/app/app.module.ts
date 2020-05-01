35b8ecfabd4db9675f509d0397eadb468b74ce6b
Kavini <kavininanayakkara@gmail.com>
2020-05-01 15:12:28 +0530

    Feedback - Reactive form

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -59,6 +62,9 @@ import { LeaderService } from './services/leader.service';
     MatCheckboxModule ,
     MatInputModule,
     MatFormFieldModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   providers: [
     DishService,
